36f91a821e9f16f2ca343f4f2d2ce333a82248a3
Bin Hu <binhu1115@gmail.com>
2023-09-13 10:08:00 +0800

    Add files via upload

--- wan_solver.py
+++ wan_solver.py
@@ -11,29 +11,28 @@ import numpy as np
 import time
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-from scipy.interpolate import griddata
-from scipy.stats import truncnorm
-from matplotlib import cm
-import torch.autograd as autograd
+
 
 class MLPBlock(nn.Module):
     def __init__(self, in_size, hidden_size, out_size):
         super(MLPBlock, self).__init__()
         self.mlp = nn.Sequential(
             nn.Linear(in_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
             nn.Softplus(),
             nn.Linear(hidden_size, out_size),
+            nn.BatchNorm1d(out_size),
+            nn.Tanh(),
         )
 
     def forward(self, x):
         x = self.mlp(x)
-        x = torch.sin(x)
+       # x = torch.sin(x)
         return x
 
 class wan_pde_solver(nn.Module):
     def __init__(self, N_dm, N_bd, beta_int, beta_bd,
-                 v_step, v_lr, u_step, u_lr, dim=2, x_range=[-1,1], t_range=[0,1], u_layer=6, u_neuron=40,
+                 v_step, v_lr, u_step, u_lr, dim=2, x_range=[-1,1], t_range=[0,1],
                  iteration=20000):
         super().__init__()
         
@@ -45,28 +44,28 @@ class wan_pde_solver(nn.Module):
         self.bd_size= N_bd                      #collocation points on domain boundary
 
         self.iteration= iteration
-        self.beta_int= beta_int
-        self.beta_bd= beta_bd
+        self.beta_int= 1
+        self.beta_bd= 2000
                     
-        self.v_step= 1                         
-        self.v_lr = 0.04  
+        self.v_step= 1                        
+        self.v_lr = 0.001  
         #                      
-        self.u_step= 2                
-        self.u_lr = 0.015
+        self.u_step= 2              
+        self.u_lr = 0.001
 
         ## integral x,t
         self.int_x = (self.x_r-self.x_l)**2
         self.int_t = self.t1-self.t0
         # Neural Net for trial function
         self.u_net = nn.Sequential(
-            MLPBlock(self.dim+1, 32, 64),
-            MLPBlock(64, 32, 16),
+            MLPBlock(self.dim+1, 32, 32),
+            MLPBlock(32, 32, 16),
             nn.Linear(16,1),
         )
         # Neural Net for test function
         self.v_net = nn.Sequential(
-            MLPBlock(self.dim+1, 32, 64),
-            MLPBlock(64, 32, 16),
+            MLPBlock(self.dim+1, 16, 32),
+            MLPBlock(32, 32, 16),
             nn.Linear(16,1),
         )
     
@@ -83,16 +82,16 @@ class wan_pde_solver(nn.Module):
         # colloaction points in the domain
         x_dm = torch.distributions.uniform.Uniform(self.x_l,self.x_r).sample([dm_size,dim])
         t_dm = torch.distributions.uniform.Uniform(self.t0, self.t1).sample([dm_size,1])
-        x_dm = torch.cat((x_dm, t_dm), dim=1)
+        xt_dm = torch.cat((x_dm, t_dm), dim=1)
         # value of u, f , dim: [x_inside_size,x_inside_size, t_size]
-        u_true = 2*torch.sin(torch.pi/2.*x_dm[:,0])*torch.cos(torch.pi/2.*x_dm[:,1])*torch.exp(-x_dm[:,-1])
+        u_true = 2*torch.sin(torch.pi/2.*xt_dm[:,0])*torch.cos(torch.pi/2.*xt_dm[:,1])*torch.exp(-xt_dm[:,-1])
         f_obv = (torch.pi**2/2.-1.) * u_true
 
         ## initial condition h(x)
-        x_init = torch.distributions.uniform.Uniform(self.x_l,self.x_r).sample([bd_size,dim])
-        x_init = torch.cat((x_init, torch.ones(bd_size, 1)* self.t0),dim=1)
-        x_end = torch.distributions.uniform.Uniform(self.x_l,self.x_r).sample([bd_size,dim])
-        x_end = torch.cat((x_end, torch.ones(bd_size, 1)* self.t1),dim=1)
+        #x_init = torch.distributions.uniform.Uniform(self.x_l,self.x_r).sample([dm_size,dim])
+        x_init = torch.cat((x_dm, torch.ones(dm_size, 1)* self.t0),dim=1)
+        #x_end = torch.distributions.uniform.Uniform(self.x_l,self.x_r).sample([dm_size,dim])
+        x_end = torch.cat((x_dm, torch.ones(dm_size, 1)* self.t1),dim=1)
 
         # value of h
         h_obv = 2*torch.sin(torch.pi/2.*x_init[:,0])*torch.cos(torch.pi/2.*x_init[:,1])
@@ -112,7 +111,7 @@ class wan_pde_solver(nn.Module):
         ## boundary of u
         bd_obv = 2*torch.sin(torch.pi/2.*x_bd[:,0])*torch.cos(torch.pi/2.*x_bd[:,1])*torch.exp(-x_bd[:,-1])
 
-        return (x_dm, x_init, x_end, x_bd, f_obv.view(-1,1), h_obv.view(-1,1), bd_obv.view(-1,1))
+        return (xt_dm, x_init, x_end, x_bd, f_obv.view(-1,1), h_obv.view(-1,1), bd_obv.view(-1,1))
 
     def sample_test(self, mesh_size, time_size, test_size, dim=2):
         '''
@@ -176,17 +175,17 @@ class wan_pde_solver(nn.Module):
         return (v_val, dv_x, dv_y, dv_t)
     
     def constraints_w(self, x_in, diff=False):
-        # decay function w = e^(-1/I/(x^2-1))  Boundary tends to 0
+        # decay function w = e^(-1/(x^2-1))/I  Boundary tends to 0
         # so test function with constraints: p = w*v
         I1 = 0.210987 #decay_coefficient
-        w = torch.exp(-1./(I1*(x_in[:,0]**2-1)*(x_in[:,1]**2-1)))
+        w = torch.exp(-1./((x_in[:,0]**2-1)*(x_in[:,1]**2-1)))/I1
         if diff:
             # dwx = 2x/(I(x**2-1)**2) * w
-            dw_x = 2.* x_in[:,0] / (I1 * (x_in[:,0]**2 -1 )**2 * (x_in[:,1]**2-1)) * w
-            dw_y = 2.* x_in[:,1] / (I1 * (x_in[:,1]**2 -1 )**2 * (x_in[:,0]**2-1)) * w
+            dw_x = 2.* x_in[:,0] / ((x_in[:,0]**2 -1 )**2 * (x_in[:,1]**2-1)) * w /I1
+            dw_y = 2.* x_in[:,1] / ((x_in[:,1]**2 -1 )**2 * (x_in[:,0]**2-1)) * w /I1
             return w.view(-1,1), dw_x.view(-1,1), dw_y.view(-1,1)
         else:
-            return w
+            return w.view(-1,1)
 
     def build(self, x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv):
         
@@ -221,7 +220,7 @@ class wan_pde_solver(nn.Module):
         ## volume of the whole domain
         int_dm = self.int_x * self.int_t
 
-        test_norm = torch.mean(torch.square(p_dm)) * int_dm
+        test_norm = torch.mean(p_dm**2) * int_dm
         ## integral term: 
         ## 
         ## 1. int du/dt * p = u * p - int dv/dt * u = u(T)*p(T) - h*p(0) - int dp/dt * u
@@ -237,9 +236,62 @@ class wan_pde_solver(nn.Module):
         loss_bd = torch.norm(u_bd-bd_obv) + torch.norm(u_init-h_obv)
 
         loss_u = self.beta_bd * loss_bd + self.beta_int * loss_int
-        loss_v = -torch.log(loss_int)
+        loss_v = -torch.log(loss_int + 1e-8)
+        return loss_u, loss_v, loss_int, loss_bd
+
+    def build2(self, x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv):
+        
+        ## initial condition u(0), v(0), p(0)
+        u_init = self.u_net(x_init)
+        v_init = self.v_net(x_init)
+        w_init = self.constraints_w(x_init[:,:-1])
+        p_init = w_init * v_init
+
+
+        ## end time u(T), v(T), p(T)
+        u_end = self.u_net(x_end)
+        v_end = self.v_net(x_end)
+        w_end = self.constraints_w(x_end[:,:-1])
+        p_end = w_end * v_end
+
+        ## boundary condition
+        u_bd = self.u_net(x_bd)
+        ## inside domain
+        u_dm, dux_dm, duy_dm, dut_dm = self.forward_u(x_dm)
+        v_dm, dvx_dm, dvy_dm, dvt_dm = self.forward_v(x_dm)
+        w_dm, dwx_dm, dwy_dm = self.constraints_w(x_dm[:,:-1],diff=True)
+        p_dm = w_dm * v_dm
+
+        ## nabla u * nabla p = nabla u * (v nabla w + w nabla v)
+        dux_dp_dm = dux_dm * (v_dm * dwx_dm + w_dm * dvx_dm)
+        duy_dp_dm = duy_dm * (v_dm * dwy_dm + w_dm * dvy_dm)
+        ## u * dp/dt = u * (w * dv/dt + v * dw/dt)
+        u_dpt = u_dm * w_dm * dvt_dm
+        ## f * p = f * w * v
+        f_p_dm = f_obv * w_dm * v_dm
+        ## volume of the whole domain
+        int_dm = self.int_x * self.int_t
+
+        test_norm = torch.mean(p_dm**2) * int_dm
+        ## integral term: 
+        ## 
+        ## 1. int du/dt * p = u * p - int dv/dt * u = u(T)*p(T) - h*p(0) - int dp/dt * u
+        int_l1 = (u_end * p_end - h_obv * p_init) * self.int_x
+        int_l2 = u_dpt * int_dm
+        ## 2. int nabla u * nabla v
+        int_l3 = (dux_dp_dm + duy_dp_dm) * int_dm
+        ## 3. int f * p
+        int_l4 = f_p_dm * int_dm
+
+        loss_int = torch.norm(int_l1 - int_l2 + int_l3 - int_l4)     
+        
+        loss_bd = torch.norm(u_bd-bd_obv) + torch.norm(u_init-h_obv)
+
+        loss_u = self.beta_bd * loss_bd + self.beta_int * loss_int
+        loss_v = loss_int
         return loss_u, loss_v, loss_int, loss_bd
     
+
     def forward(self, x):
         u = self.u_net(x)
         return u
@@ -271,12 +323,12 @@ class wan_pde_solver(nn.Module):
 
             # alternating training
             for _ in range(self.v_step):
-                loss_u, loss_v, loss_int, loss_bd = self.build(x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv)
+                loss_u, loss_v, loss_int, loss_bd = self.build2(x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv)
                 self.v_opt.zero_grad()
                 loss_v.backward(retain_graph=True)
                 self.v_opt.step()
             for _ in range(self.u_step):
-                loss_u, loss_v, loss_int, loss_bd = self.build(x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv)
+                loss_u, loss_v, loss_int, loss_bd = self.build2(x_dm, x_init, x_end, x_bd, f_obv, h_obv, bd_obv)
                 self.u_opt.zero_grad()
                 loss_u.backward(retain_graph=True)
                 self.u_opt.step()
@@ -284,10 +336,10 @@ class wan_pde_solver(nn.Module):
             opt_time = time.time() - optimizer_start
             print("{:.2f} s to optimizer| loss_u {:6.3f}, loss_v {:6.3f}.".format(opt_time, loss_u, loss_v))
             ## test step ##
-            if itr % 5==0:
+            if itr % 1==0:
                 pred_u = self.forward(x_test)
                 err_l2= torch.norm(pred_u-u_test)
-                print(f'loss_u:{loss_u} loss_v:{loss_v} loss_int:{loss_int} loss_bd:{loss_bd} test_norm:{err_l2}')
+                print("loss_u:{:6.3f} loss_v:{:6.3f} loss_int:{:6.3f} loss_bd:{:6.3f} test_norm:{:6.3f}".format(loss_u, loss_v, loss_int, loss_bd, err_l2))
 
     
 if __name__ == '__main__':
